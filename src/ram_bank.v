24c618f7fb2fa0d5269f828b685b882076fcd28d
Yucan Liu <yucan@dyn-160-39-143-238.dyn.columbia.edu>
2018-05-02 19:38:05 -0400

    ram test

---
+++ src/ram_bank.v
@@ -0,0 +1,33 @@
+`timescale 1ns / 1ps
+
+module ram_bank(clk, en, we, re, addr_w, d_w, addr_r, d_r);
+
+  parameter ADDR_BIT = 3;
+  parameter DATA_BIT = 16;
+  parameter MEM_HEIGHT = 8;
+
+  input clk, en, we, re;
+  input [DATA_BIT-1 : 0] d_w;
+  input [ADDR_BIT-1 : 0] addr_w;
+
+  output [DATA_BIT - 1 : 0] d_r;
+  output [ADDR_BIT-1 : 0] addr_r;
+
+  reg [DATA_BIT-1 : 0] mem [0 : MEM_HEIGHT-1];
+  reg [DATA_BIT-1 : 0] d_r_o;
+
+  always @ ( posedge clk ) begin
+
+    if (en) begin
+      if (we) begin
+        mem[addr_w] <= d_w;
+      end
+      if (re) begin
+        d_r_o <= mem[addr_r];
+      end
+      assign d_r = d_r_o;
+    end
+
+  end
+
+endmodule // ram_bank
---
+++ tb/ram_bank_tb.v
@@ -0,0 +1,58 @@
+`timescale 1ns / 1ps
+
+module testbench();
+
+  parameter ADDR_BIT = 3;
+  parameter DATA_BIT = 16;
+  parameter MEM_HEIGHT = 8;
+
+  reg clk, en, we, re;
+  reg [DATA_BIT-1 : 0] d_w;
+  reg [ADDR_BIT-1 : 0] addr_w;
+
+  reg [DATA_BIT - 1 : 0] d_r;
+  reg [ADDR_BIT-1 : 0] addr_r;
+
+  ram_bank r1 (clk, en, we, re, addr_w, d_w, addr_r, d_r);
+
+  initial begin
+    clk = 0;
+    en = 0;
+    we = 0;
+    re = 0;
+    #50;
+    en = 1;
+    we = 1;
+    re = 0;
+
+    addr_w = 0;
+    d_w = 0;
+    #10
+    addr_w = 1;
+    d_w = 1;
+    #10
+    addr_w = 2;
+    d_w = 2;
+    #10
+    addr_w = 3;
+    d_w = 3;
+    #10
+    addr_w = 4;
+    d_w = 4;
+    #10
+    addr_w = 5;
+    d_w = 5;
+    #10
+    addr_w = 6;
+    d_w = 6;
+    #10
+    addr_w = 7;
+    d_w = 7;
+    #50;
+
+  end
+
+  always
+    #5 clk = ~clk;
+
+endmodule
